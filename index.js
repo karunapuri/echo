32e4958816442afabfc0247c7b89548ce30215bd
karunapuri <karunapuri@users.noreply.github.com>
2018-01-31 15:54:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const restService = express();
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'karuna.puri@springernature.com',
-  from: 'karuna.puri@springernature.com',
+  to: 'test@example.com',
+  from: 'test@example.com',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
