955d4251a2ce5ec18cf499384b081bb6a5f8913a
karunapuri <karunapuri@users.noreply.github.com>
2018-01-31 15:35:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -201,6 +201,17 @@ restService.post("/slack-test", function(req, res) {
   });
 });
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'karuna.puri@springernature.com',
+  from: 'karuna.puri@springernature.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
